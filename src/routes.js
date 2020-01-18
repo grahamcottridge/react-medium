7cd6d7429d33738a482d4cff7bc7e074d6a4eb8d
Graham Cottridge <codinggc@gmail.com>
2020-01-18 23:48:17 +0000

    Add authentication component

--- src/routes.js
+++ src/routes.js
@@ -3,12 +3,14 @@ import { Switch, Route } from "react-router-dom";
 
 import Home from "./pages/home/Home.component";
 import Article from "./pages/article/Article.component";
+import Authentication from "./pages/authentication/Authentication.component";
 
 const Routes = () => {
   return (
     <Switch>
       <Route path="/" component={Home} exact />
-      <Route path="/articles/:slug" conponent={Article} />
+      <Route path="/articles/:slug" component={Article} />
+      <Route path="/signin" component={Authentication} />
     </Switch>
   );
 };
